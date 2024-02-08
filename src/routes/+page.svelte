9718e1ba52e5cbcaf01b74cda74641db5a89e5cf
l-bianchi <illorenzobianchi@proton.me>
2024-02-08 14:21:47 +0100

    add greet command and component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,6 @@
+<script>
+  import Greet from "../lib/Greet.svelte";
+</script>
+
 <h1>Welcome to SvelteKit</h1>
-<p>
-  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
-</p>
+<Greet />
